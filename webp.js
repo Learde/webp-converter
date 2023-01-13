6b809f81cfb4e3e6cf5da744cc916d1fb170801a
learde <leardeleo@gmail.com>
2023-01-14 00:45:33 +0300

    Init project

---
+++ webp.js
@@ -0,0 +1,24 @@
+const webp=require('webp-converter');
+const fs = require('fs');
+const folder = 'images/';
+const output = 'webp/';
+
+fs.readdir(folder, (err, files) => {
+  files.forEach(file => {
+    const fileWithoutFormat = file.split('.')[0];
+
+    const result = webp.cwebp(folder + file, output + fileWithoutFormat + '.webp',"-q 0",logging="-v");
+    result.then((response) => {
+        console.log(response);
+    });
+  });
+});
+//pass input image(.jpeg,.pnp .....) path ,output image(give path where to save and image file name with .webp extension)
+//pass option(read  documentation for options)
+
+//cwebp(input,output,option)
+
+const result = webp.cwebp("images/main-asset-1.jpg","webp/main-asset-1.webp","-q 92",logging="-v");
+result.then((response) => {
+  console.log(response);
+});
